aaa636479df7241b3160c0d243131044fe0fa20b
André Kurpanek <21091182+akurpanek@users.noreply.github.com>
2025-12-07 00:34:13 +0100

    Create add-flatpak-remotes.sh

---
+++ files/scripts/add-flatpak-remotes.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+echo 'Add remote repositories to flatpak configuration'
+
+# Download the GPG keys using curl
+curl -sL "https://downloads.1password.com/linux/flatpak/repo/onepassword.asc" | \
+  gpg --dearmor -o "/usr/share/keyrings/1password-archive-keyring.gpg"
+
+# Add the Flatpak remote repository
+flatpak remote-add \
+    --if-not-exists \
+    --no-enumerate \
+    --gpg-verify-key="/usr/share/keyrings/1password-archive-keyring.gpg" \
+    "onepassword-origin" \
+    "https://downloads.1password.com/linux/flatpak/repo/"
