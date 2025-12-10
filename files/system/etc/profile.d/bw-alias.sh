491e4d7b19365a0e0f08fe6d98f1d6a0d14915f5
André Kurpanek <21091182+akurpanek@users.noreply.github.com>
2025-12-10 03:07:33 +0100

    Update bw-alias.sh

--- files/system/etc/profile.d/bw-alias.sh
+++ files/system/etc/profile.d/bw-alias.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-# Check if Bitwarden Flatpak is installed
-if flatpak list --app | grep -q "com.bitwarden.desktop"; then
-    : # TODO: Uncomment the alias if Bitwarden CLI via Flatpak is needed
-    # alias bw='flatpak run --command=bw com.bitwarden.desktop'
+#
+# Sets an alias for Bitwarden CLI if the 'bw' binary is not found in PATH and
+# Bitwarden Flatpak is installed. This allows using 'bw' via Flatpak as a
+# fallback when no native binary is available.
+
+if ! command -v bw >/dev/null 2>&1 &&
+   flatpak list --app | grep -q "com.bitwarden.desktop"; then
+  alias bw='flatpak run --command=bw com.bitwarden.desktop'
 fi
